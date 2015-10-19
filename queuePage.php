7a5b129fa32ca54548b1f5703bea53a11ab7210e
Risgn94 <asger.thyregod@gmail.com>
2015-10-19 20:06:58 +0200

    Hej

--- queuePage.php
+++ queuePage.php
@@ -1,22 +1,6 @@
 
 <script type="text/javascript">
-$(document).ready(function() {
-        function updateDiv(){
-            $.ajax({
-                url: "./Json/generateCurrentNumber.php",
-                dataType: "json",
-                cache: false,
-                success: function(data) {
-                    console.log(data);
-                    document.getElementById('numberDisplay').innerHTML = 'Current number being serviced: ' + data[0];
-                    //$('#trade_pp').html(data.trade);
-                    //$('#ect_pp').html(data.ect);
-                }             
-            });
-        }
-        updateDiv();
-        setInterval(updateDiv, 10000);
-    });
+
 
 </script>
 
@@ -26,8 +10,46 @@ $(document).ready(function() {
 <h1>Queue progress </h1>
 
 <div class="progress progress-striped">
-    <div class="progress-bar progress-bar-success" style="width: 40%"></div>
+    <div class="progress-bar progress-bar-success" style="width: 50%"></div>
 </div>
 
 <h3 id="numberDisplay" > </h3>
-
+<script>
+    var globalCounter = 0;
+    var restCounter = 0;
+    // Hvis AJAX kaldet er lig med det nummer som allerede bliver serviceret så skal der intet ske.
+ function CalculateWaitingTime(x, y){
+     globalCounter = x - y;
+     
+ }
+ $(document).ready(function() {
+        function updateDiv(){
+            $.ajax({
+                url: "./Json/generateCurrentNumber.php",
+                dataType: "json",
+                cache: false,
+                success: function(data) {
+                    console.log(data);
+                    if(document.getElementById('numberDisplay').innerHTML == data[0][currentServed])
+                    {
+                        //Do nothing
+                    }
+                    else
+                    {
+                      temp = data[0][currentServed] - document.getElementById('numberDisplay').innerHTML;
+                      restCounter=+temp;
+                        document.getElementById('numberDisplay').innerHTML = data[0][currentServed];
+                        document.getElementsByClassName('progress-bar progress-bar-success')[0].style.width = CalculateWaitingTime(2, 5);
+                        
+                    }
+                    document.getElementById('numberDisplay').innerHTML = 'Current number being serviced: ' + data[0];
+                    //$('#trade_pp').html(data.trade);
+                    //$('#ect_pp').html(data.ect);
+                }             
+            });
+        }
+        updateDiv();
+        setInterval(updateDiv, 10000);
+    });
+     </script>
+ 
